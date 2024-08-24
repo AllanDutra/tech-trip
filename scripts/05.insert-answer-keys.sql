8b0e18abc01affc28f04364d1d9669c4386a6f1c
allandutra <allandutrafeitosa14@gmail.com>
2024-08-24 16:34:22 -0300

    feat: insert into answer keys

---
+++ scripts/05.insert-answer-keys.sql
@@ -0,0 +1,224 @@
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'TERRESTRE-TERRESTRE-AQUATICO-AQUATICO',
+        'GRUPO1-GRUPO1-GRUPO2-GRUPO2',
+        'A resposta é a concatenação das strings de descrição das figuras da atividade, na ordem da esquerda para a direita e de cima para baixo. Cada descrição deve ser separada por "-" na concatenação.',
+        1
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '17',
+        'Número inteiro igual ao total de passos executados',
+        'Cada estudante terá apenas uma tentativa. A pontuação é calculada pelo número de passos executados, ou seja, quanto mais próximo chegar do menor número de passos (17), maior será sua pontuação. Será levada em consideração uma margem de erro de 3 passos, ou seja, se o aluno fez 20 passos = 3 estrelas, 23 passos = 2 estrelas e 26+ passos = 1 estrela.',
+        2
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'HARDWARE-HARDWARE-SOFTWARE-SOFTWARE',
+        'GRUPO1-GRUPO1-GRUPO2-GRUPO2',
+        'A resposta é a concatenação das strings de descrição das figuras da atividade, na ordem da esquerda para a direita e de cima para baixo. Cada descrição deve ser separada por "-" na concatenação.',
+        3
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '0',
+        'Valor bit indicando se é true (1) ou false (0)',
+        'Valor indicando se a frase é verdadeira (1) ou falsa (0).',
+        4
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '3',
+        'Número inteiro igual ao total de passos executados',
+        'O número de passos para concluir a atividade será sempre igual a 3, então todos os estudantes sempre conquistarão 3 estrelas nesse desafio.',
+        5
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '1',
+        'Número int igual ao total de tentativas até vencer',
+        'Número inteiro que representa quantas tentativas o estudante precisou para vencer o jogo da velha contra a máquina. A margem de erro será de 2, e a resposta perfeita será 1. Logo, se o estudante vencer em até 3 tentativas, pontuará 3 estrelas; até 5 tentativas, pontuará 2 estrelas; e 7+ tentativas, pontuará apenas 1 estrela.',
+        6
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '4',
+        'Número inteiro igual ao total de passos executados',
+        'O número de passos para concluir a atividade será sempre igual a 4, então todos os estudantes sempre conquistarão 3 estrelas nesse desafio.',
+        7
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '6',
+        'Número inteiro igual ao total de passos executados',
+        'O número de passos para concluir a atividade será sempre igual a 6, então todos os estudantes sempre conquistarão 3 estrelas nesse desafio.',
+        8
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'C',
+        'Alternativa selecionada entre A, B e C',
+        'O desafio consiste em uma pergunta com 3 alternativas. A pontuação será calculada com base no número de tentativas até acertar, com margem de erro igual a 1. Logo, para alcançar a pontuação máxima, precisará de 1 tentativa; 2 tentativas para 2 estrelas; e 3+ tentativas para 1 estrela.',
+        9
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'B',
+        'Alternativa selecionada entre A, B e C',
+        'O desafio consiste em uma pergunta com 3 alternativas. A pontuação será calculada com base no número de tentativas até acertar, com margem de erro igual a 1. Logo, para alcançar a pontuação máxima, precisará de 1 tentativa; 2 tentativas para 2 estrelas; e 3+ tentativas para 1 estrela.',
+        10
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'JOAO-ESTER',
+        'POSINFERIORDOGRAFO-POSDIREITADOGRAFO',
+        'O desafio consiste em completar o grafo, com a posição inferior e a direita faltantes. O estudante deve mover o personagem João para a posição inferior e a personagem Ester para a posição da direita do grafo.',
+        11
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'E-NAO-OU',
+        'Strings das operações lógicas separadas por "-"',
+        'O desafio consiste em mover as operações lógicas para seus devidos quadros seguindo a ordem de cima para baixo, onde no primeiro quadrado vazio deve-se mover a conjunção "E", no segundo a negação "NÃO" e no terceiro a disjunção "OU".',
+        12
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'A-B-B',
+        'Alt. das 3 perguntas separadas por "-"',
+        'O desafio consiste em 3 perguntas com o mesmo objetivo final. O aluno precisa responder corretamente cada uma das perguntas. O menor número de tentativas para concluir o desafio é 3. A margem de erro será de 2. Logo, se ele levou no máximo 5 tentativas para acertar todas, pontuará 3 estrelas; se levou 7 tentativas, pontuará 2 estrelas; se levou 9 tentativas ou mais, pontuará 1 estrela.',
+        13
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        '2',
+        'Número inteiro igual ao total de passos executados',
+        'O número de passos para concluir a atividade será sempre igual a 2, então todos os estudantes sempre conquistarão 3 estrelas nesse desafio.',
+        14
+    );
+
+INSERT INTO
+    public."AnswerKeys"(
+        "Response",
+        "ResponsePattern",
+        "Help",
+        "Challenge_Id"
+    )
+VALUES
+    (
+        'B',
+        'Alternativa selecionada entre A, B e C',
+        'O desafio consiste em uma pergunta com 3 alternativas. A pontuação será calculada com base no número de tentativas até acertar, com margem de erro igual a 1. Logo, para alcançar a pontuação máxima, precisará de 1 tentativa; 2 tentativas para 2 estrelas; e 3+ tentativas para 1 estrela.',
+        15
+    );
