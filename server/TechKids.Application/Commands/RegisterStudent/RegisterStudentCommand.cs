940c75c3926b886ca9c5dd1d597d5b13ac271d92
allandutra <allandutrafeitosa14@gmail.com>
2024-08-28 21:29:58 -0300

    refactor: adapt to student base validator

--- server/TechKids.Application/Commands/RegisterStudent/RegisterStudentCommand.cs
+++ server/TechKids.Application/Commands/RegisterStudent/RegisterStudentCommand.cs
@@ -1,15 +1,10 @@
 using MediatR;
+using TechKids.Core.Models.InputModels;
 
 namespace TechKids.Application.Commands
 {
-    public class RegisterStudentCommand : IRequest<int>
+    public class RegisterStudentCommand : StudentBaseInputModel, IRequest<int>
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string User { get; set; }
         public string Password { get; set; }
-        public DateTime Birth { get; set; }
-        public string Gender { get; set; }
-        public short Character_Id { get; set; }
     }
 }
--- server/TechKids.Application/Validators/RegisterStudentCommandValidator.cs
+++ server/TechKids.Application/Validators/RegisterStudentCommandValidator.cs
@@ -4,58 +4,16 @@ using TechKids.Shared.Utils;
 
 namespace TechKids.Application.Validators
 {
-    public class RegisterStudentCommandValidator : AbstractValidator<RegisterStudentCommand>
+    public class RegisterStudentCommandValidator : StudentBaseValidator<RegisterStudentCommand>
     {
         public RegisterStudentCommandValidator()
         {
-            RuleFor(p => p.Name.Trim())
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("Por favor, informe o nome do estudante!");
-
-            RuleFor(p => p.Email)
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("Por favor, informe um endereço de e-mail!")
-                .Must(Validations.IsValidEmail)
-                .WithMessage("Por favor, informe um endereço de e-mail válido!");
-
-            RuleFor(p => p.User)
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("Por favor, informe um nome de usuário!")
-                .Must(Validations.IsValidUsername)
-                .WithMessage(
-                    "Por favor, informe um nome de usuário válido. (Sem _ ou . no final ou no início; Sem __ ou _. ou ._ ou .. em sequência; Entre 8 a 50 caracteres.)"
-                );
-
             RuleFor(p => p.Password.Trim())
                 .NotNull()
                 .NotEmpty()
                 .WithMessage("Por favor, informe uma senha!")
                 .Must(password => !Validations.ContainsSpaces(password))
                 .WithMessage("A senha não pode conter espaços.");
-
-            RuleFor(p => p.Birth)
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("Por favor, informe a data de nascimento!")
-                .LessThanOrEqualTo(DateTime.Now.Date)
-                .WithMessage("A data de nascimento não pode ser maior do que a data atual!");
-
-            RuleFor(p => p.Gender)
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("Por favor, informe o gênero!")
-                .Must(Validations.IsValidGender)
-                .WithMessage(
-                    "Por favor, informe um gênero válido. Utilize 'male' para masculino ou 'female' para feminino."
-                );
-
-            RuleFor(p => p.Character_Id)
-                .NotNull()
-                .NotEmpty()
-                .WithMessage("Por favor, informe o id do personagem!");
         }
     }
 }
