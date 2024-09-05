ab0ed8da0e95a19b296078ed01db8fcdefcaabea
AllanDutra <allandutrafeitosa14@gmail.com>
2024-09-05 20:16:12 -0300

    feat: implementation of process for attempt 2

---
+++ server/TechKids.Core/Factories/AnswerKeys/ProcessAttempt2DomainService.cs
@@ -0,0 +1,65 @@
+using System.Net;
+using TechKids.Core.Interfaces.Factories;
+using TechKids.Core.Interfaces.Notifications;
+using TechKids.Core.Models.InputModels;
+using TechKids.Core.Models.ViewModels;
+
+namespace TechKids.Core.Factories.AnswerKeys
+{
+    public class ProcessAttempt2DomainService : IProcessAttemptDomainService
+    {
+        private readonly INotifier _notifier;
+
+        public ProcessAttempt2DomainService(INotifier notifier)
+        {
+            _notifier = notifier;
+        }
+
+        public ProcessedAttemptProductViewModel? Process(ProcessAttemptInputModel inputModel)
+        {
+            bool successToConvert = short.TryParse(
+                inputModel.AnswerKey_Response,
+                out short betterStepsNumberAsInt
+            );
+
+            if (!successToConvert)
+            {
+                _notifier.Handle(
+                    "Tivemos um problema interno no servidor ao tentar converter a resposta do desafio 2.",
+                    HttpStatusCode.InternalServerError
+                );
+
+                return null!;
+            }
+
+            short marginOfError = 3;
+
+            short totalStarsEarned;
+
+            if (inputModel.Steps < betterStepsNumberAsInt)
+            {
+                _notifier.Handle(
+                    $"Número de passos inválido. O menor número de passos possível é {betterStepsNumberAsInt}.",
+                    HttpStatusCode.BadRequest
+                );
+
+                return null!;
+            }
+
+            if (inputModel.Steps <= (betterStepsNumberAsInt + marginOfError))
+            {
+                totalStarsEarned = 3;
+            }
+            else if (inputModel.Steps <= (betterStepsNumberAsInt + (marginOfError * 2)))
+            {
+                totalStarsEarned = 2;
+            }
+            else
+            {
+                totalStarsEarned = 1;
+            }
+
+            return new(true, totalStarsEarned);
+        }
+    }
+}
