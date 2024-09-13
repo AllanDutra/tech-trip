32acc3acd6f31698d4c9c3a07e1c89d0b2cdb04c
allandutra <allandutrafeitosa14@gmail.com>
2024-09-13 20:48:07 -0300

    feat: greetings component

---
+++ web/src/app/shared/components/Greetings/styles.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledGreetings = styled.div`
+  color: #424243;
+
+  display: flex;
+  align-items: center;
+  gap: 1em;
+
+  div:nth-of-type(1) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+
+    gap: 0.125em;
+  }
+
+  span {
+    font-family: "Montserrat Alternates", sans-serif;
+    font-weight: 500;
+    font-size: 0.85em;
+  }
+
+  strong {
+    font-family: "Lilita One", sans-serif;
+    font-weight: 400;
+    font-size: 1.625em;
+    letter-spacing: 0.0325em;
+  }
+`;
