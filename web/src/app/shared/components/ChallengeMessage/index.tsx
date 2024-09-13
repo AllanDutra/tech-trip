cb5e151810c30fc0385bbadeb6e1e58e7a97bdaf
allandutra <allandutrafeitosa14@gmail.com>
2024-09-13 20:58:38 -0300

    refactor: add challenge message html span properties

--- web/src/app/shared/components/ChallengeMessage/index.tsx
+++ web/src/app/shared/components/ChallengeMessage/index.tsx
@@ -1,10 +1,17 @@
 import { ReactNode } from "react";
 import { StyledChallengeMessage } from "./styles";
 
-interface IChallengeMessageProps {
+interface IChallengeMessageProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLSpanElement>,
+    HTMLSpanElement
+  > {
   children: ReactNode;
 }
 
-export function ChallengeMessage({ children }: IChallengeMessageProps) {
-  return <StyledChallengeMessage>{children}</StyledChallengeMessage>;
+export function ChallengeMessage({
+  children,
+  ...rest
+}: IChallengeMessageProps) {
+  return <StyledChallengeMessage {...rest}>{children}</StyledChallengeMessage>;
 }
